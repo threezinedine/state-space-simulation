cfb5490394a63022398ef2514941fa4c639fd3ef
unknown <threezinedine@gmail.com>
2022-08-06 22:48:45 +0700

    add test for feedback controller

--- models/signals/full_state_feedback_controller.py
+++ models/signals/full_state_feedback_controller.py
@@ -4,12 +4,22 @@ from ..timer import ITimer
 from .i_feedback import IFeedBack
 
 
+DTYPE = np.float32
+
+
 class FullStateFeedBackController(Signal):
     def __init__(self, timer:ITimer, feedback_model:IFeedBack, gain_param:np.ndarray=None):
         Signal.__init__(self, timer)
         self._feedback_model = feedback_model 
+
         self._gain_param = gain_param
 
-    def get_input(self):
+    def get_input(self, dtype=DTYPE):
         self._index += 1
-        return self._feedback_model.get_feedback()
+        state = self._feedback_model.get_feedback()
+
+        if self._gain_param is None:
+            self._gain_param = np.ones_like(state, dtype=dtype)
+            
+        result = np.dot(state.reshape(1, -1), self._gain_param.reshape(-1, 1))
+        return result.astype(dtype)
--- tests/test_full_state_feedback_controller.py
+++ tests/test_full_state_feedback_controller.py
@@ -12,7 +12,9 @@ DTYPE = np.float32
 class TestFullStateFeedbackController(unittest.TestCase):
     @parameterized.expand([
             [np.array([[1., 1.]]), np.array([[2., 3.]]), np.array([[5.]]), DTYPE],
-            [np.array([[1., 1.]]), None, np.array([[2.]]), DTYPE] 
+            [np.array([[1., 1.]]), None, np.array([[2.]]), DTYPE],
+            [np.array([[2., 1.]]), None, np.array([[3.]]), DTYPE],
+            [np.array([[2., 1.]]), None, np.array([[3.]]), np.float64] 
         ]) 
     def test_get_input_method(self, input_state, gain_param, expected, dtype):
         timer = Mock(spec=ITimer)
@@ -22,7 +24,8 @@ class TestFullStateFeedbackController(unittest.TestCase):
 
         controller = FullStateFeedBackController(timer, feedback_model, gain_param)
 
-        output = controller.get_input()
+        output = controller.get_input(dtype=dtype)
 
         assert isinstance(output, np.ndarray)
+        assert output.dtype == dtype
         self.assertListEqual(output.tolist(), expected.tolist())
